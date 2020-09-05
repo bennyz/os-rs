a9defd7324eeb7d29ad4f03952c0bd4533b40957
Benny Zlotnik <bzlotnik@protonmail.com>
2020-09-05 11:04:05 +0300

    finish hello world boot image

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; // Convert the address into a raw pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // Access memory at the relevant offset and place the byte representing the character
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // The second byte represents the color (cyan in this case)
+        }
+    }
+
     loop {}
 }
